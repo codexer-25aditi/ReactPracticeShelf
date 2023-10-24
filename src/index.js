9c5f33201dc8ba308464fffddf091385ef5fd8a0
ADITI SAHU <88385979+codexer-25aditi@users.noreply.github.com>
2023-10-24 15:50:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// var React = require("react");
+// var ReactDOM=require("react-dom");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>HelloWorld!</h1>,document.getElementById("root"));
