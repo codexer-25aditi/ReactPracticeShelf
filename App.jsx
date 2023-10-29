b49378e92978daee41db6f630a102e642a9f80b2
ADITI SAHU <88385979+codexer-25aditi@users.noreply.github.com>
2023-10-29 22:29:24 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import list from "./list.jsx";
+
+function App() {
+  return;
+  <div>
+    <Heading />
+    <list />
+  </div>;
+}
+
+export default App;
